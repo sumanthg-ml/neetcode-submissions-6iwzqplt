6e7eb17e01f9ea6d74a63e67a7b67edcf69a7d53
sumanthg-ml <67048663+sumanthg-ml@users.noreply.github.com>
2026-04-22 16:52:15 +0530

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let left = 0, right = height.length-1;
+        let maxLeft = 0, maxRight = 0;
+        let water = 0;
+
+        while(left < right){
+            if(height[left] <= height[right]){
+                if(height[left] >= maxLeft){
+                    maxLeft = height[left];
+                }
+                else{
+                    water += maxLeft - height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right] >= maxRight){
+                    maxRight = height[right];
+                }
+                else{
+                    water += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
+}
