2c683d650c35834790b0bbc057191eaeb286347b
sumanthg-ml <67048663+sumanthg-ml@users.noreply.github.com>
2026-04-23 21:51:23 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let left = 0, right=heights.length-1;
+        let max = 0;
+        while(left < right){
+            const area = Math.min(heights[left], heights[right]) * (right-left);
+            max = Math.max(max, area);
+
+            if(heights[left] <= heights[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return max;
+    }
+}
