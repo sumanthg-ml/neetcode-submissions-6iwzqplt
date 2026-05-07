e78b789ca972be1f56557748c401e33dd32d5b2f
sumanthg-ml <67048663+sumanthg-ml@users.noreply.github.com>
2026-05-07 22:13:31 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String s: strs){
+            sb.append(s.length()).append("#").append(s);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()){
+            int j = i;
+            while(str.charAt(j) != '#'){
+                j++;
+            }
+            int len = Integer.parseInt(str.substring(i,j));
+            i = j+1;
+            res.add(str.substring(i, i+len));
+            i += len;
+        }
+        return res;
+    }
+}
